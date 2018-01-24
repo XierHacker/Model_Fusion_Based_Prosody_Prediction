7e6f47a126f69594b477f85750dd354e42f6eff9
XierHacker <xierhacker@163.com>
2018-01-24 13:23:56 +0800

    add read extra information in util

--- util.py
+++ util.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import parameter
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
-import parameter
+from sklearn.preprocessing import OneHotEncoder
 
 #compute accuracy,precison,recall and f1
 def eval(y_true,y_pred):
@@ -14,7 +15,6 @@ def eval(y_true,y_pred):
     #f1-score
     f_1=f1_score(y_true=y_true,y_pred=y_pred,average=None)
     return accuracy,f_1
-
 
 def getTag2(preds_pw,preds_pph):
     # get complex "#" index
@@ -36,17 +36,8 @@ def getTag2(preds_pw,preds_pph):
     arg = (arg / 2).astype(dtype=np.int32)
     return arg
 
-#def charBasedTag2(X,preds_pw,preds_pph):
-    # get id2words
-#    df_words_ids = pd.read_csv(filepath_or_buffer="../data/dataset/temptest/words_ids.csv", encoding="utf-8")
-    # print(df_words_ids.head(5))
-#    id2words = pd.Series(data=df_words_ids["words"].values, index=df_words_ids["id"].values)
-    # print(id2words[2])
 
-    #
-
-
-#recover to original result
+#recover to .txt format
 def recover2(X,preds_pw,preds_pph,filename):
     arg=getTag2(preds_pw,preds_pph)
     arg=np.reshape(arg,newshape=(-1,parameter.MAX_SENTENCE_SIZE))   #[test_size,max_sentence_size]
@@ -73,14 +64,38 @@ def recover2(X,preds_pw,preds_pph,filename):
     f.write(doc)
     f.close()
 
+#read extra info form file,like pos info of word,or position info etc...
+#trans to one-hot format
+def readExtraInfo(file):
+    f = open(file=file, encoding="utf-8")
+    lines = f.readlines()
+    #print("lines numbers:",len(lines))
+    X=np.zeros(shape=(len(lines),parameter.MAX_SENTENCE_SIZE),dtype=np.int32)
+    i = 0
+    for line in lines:
+        # print(line)
+        line = line.strip()
+        line_list = line.split(sep=" ")
+        # print(line_list)
+        j = 0
+        for id in line_list:
+            X[i, j] = id
+            j += 1
+        i += 1
+    #print(X)
+    #print("word_id_train.shape", X.shape)
+    return X
+
+    #one-hot encoder
+
 
 if __name__ =="__main__":
-    #测试
-    a=np.array([1,2,3,4,0,5,6,7,1,1,2,1,0])
-    print(a)
-    result=binarize(sequence=a,positive_value=1)
-    print(result)
-    print(a)
+    print("read extra info test:")
+    #readExtraInfo(file="./data/dataset/pos_train_tag.txt")
+    #readExtraInfo(file="./data/dataset/pos_test_tag.txt")
+
+    #readExtraInfo(file="./data/dataset/length_train_tag.txt")
+    #readExtraInfo(file="./data/dataset/length_test_tag.txt")
 
 
 
